6422d69f4cc3f4f932b4c66edaf59fbc8f8ab05f
MayMoeOo95 <122252464+MayMoeOo95@users.noreply.github.com>
2023-03-16 11:13:31 +0800

    Deleted createReview
    Deleted the old createReview function from controller

--- controllers/reviewsC.js
+++ controllers/reviewsC.js
@@ -6,23 +6,11 @@ const Recipe = require("../models/RecipeM");
  * @returns
  */
 
-const createReview1 = async (req, res) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id).exec();
-    recipe.review.push(req.body);
-    await recipe.save();
-    res.redirect(`/recipes/${recipe._id}`);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error in Creating reviews");
-  }
-};
-
 const createReview = async (req, res) => {
   try {
     const recipe = await Recipe.findById(req.params.id).exec();
     const review = {
-      userName: req.session.user.userName, // set the username of the logged-in user as the reviewer name
+      userName: req.session.user.userName,
       content: req.body.content,
     };
     console.log("review username is ", review.userName);
